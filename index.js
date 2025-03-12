928df2e947b79bd4983ac5728b376d620a766df2
Manoj kumar <manoj166174@gmail.com>
2025-03-12 19:36:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Table</title>
+</head>
+<body>
+    <h2>Student Details</h2>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Course</th>
+        </tr>
+        <tr>
+            <td>John</td>
+            <td>21</td>
+            <td>IT</td>
+        </tr>
+        <tr>
+            <td>Emma</td>
+            <td>22</td>
+            <td>CS</td>
+        </tr>
+    </table>
+</body>
+</html>
